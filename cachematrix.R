79cad6f8f18858b39d35f772e4539ccd18f5920a
IsraelA777 <se.israelabebe@gmail.com>
2016-03-22 11:48:05 +0300

    makeCacheMatrix function done

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,18 @@
 ## get/set value of matrix
 ## get/set value of inverse
 makeCacheMatrix <- function(x = matrix()) {
-	
+
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x 
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
